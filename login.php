f92501463fed61f7f977bb723eb9e9f75ace6998
masataka-creator <masataka.asano.0101@gmail.com>
2021-01-19 14:33:18 +0900

    ログイン情報をCookieに保存

--- login.php
+++ login.php
@@ -1,3 +1,45 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if ($_COOKIE['number'] !== '') {
+  $number = $_COOKIE['number'];
+}
+
+if(!empty($_POST)) {
+  $number = $_POST['number'];
+  
+  if (strlen($_POST['number']) >6) {
+    $error['number'] = 'small';
+  }
+  if (strlen($_POST['password']) <4) {
+    $error['password'] = 'length';
+  }
+  if ($_POST['number'] !== '' && $_POST['password'] !== '') {
+$login = $db->prepare('SELECT * FROM members WHERE number=? AND password=?');
+$login->execute(array(
+  $_POST['number'],
+  sha1($_POST['password'])
+));
+$member = $login->fetch();
+
+if ($member) {
+  $_SESSION['id'] = $member['id'];
+  $_SESSION['time'] = time();
+
+  if ($_POST['save'] === 'on') {
+    setcookie('number', $_POST['number'], time()+60*60*24*14);
+  }
+  header('Location: index.php');
+  exit();
+} else {
+  $error['login'] = 'failed';
+}
+  } else {
+    $error['login'] = 'blank';
+  }
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -13,19 +55,31 @@
   </div>
   <div id="content">
     <div id="lead">
-      <p>メールアドレスとパスワードを記入してログインしてください。</p>
+      <p>社員番号とパスワードを記入してログインしてください。</p>
       <p>入会手続きがまだの方はこちらからどうぞ。</p>
       <p>&raquo;<a href="join/">入会手続きをする</a></p>
     </div>
     <form action="" method="post">
       <dl>
-        <dt>メールアドレス</dt>
+        <dt>社員番号</dt>
         <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+          <input type="number" name="number" size="35" maxlength="255" value="<?php echo htmlspecialchars($number, ENT_QUOTES); ?>" />
+          <?php if ($error['login'] === 'blank'): ?>
+					<p class="error">*社員番号とパスワードをご記入下さい</p>
+					<?php endif; ?>
+          <?php if ($error['login'] === 'failed'): ?>
+					<p class="error">*ログインに失敗しました。正しくご記入下さい</p>
+					<?php endif; ?>
+					<?php if ($error['number'] === 'small'): ?>
+					<p class="error">*社員番号は6文字以内の数字で入力して下さい</p>
+					<?php endif; ?>
         </dd>
         <dt>パスワード</dt>
         <dd>
-          <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password']); ?>" />
+          <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>" />
+					<?php if ($error['password'] === 'length'): ?>
+					<p class="error">*パスワードは4文字以上で入力して下さい</p>
+					<?php endif; ?>
         </dd>
         <dt>ログイン情報の記録</dt>
         <dd>
