fbd1eb5e389d8a4175a639d9d6a2e61b69f4f937
masataka-creator <masataka.asano.0101@gmail.com>
2021-02-02 22:53:09 +0900

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '388e44e9783311af21eb4ed4b70fb7f9023e916c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '388e44e9783311af21eb4ed4b70fb7f9023e916c',
+    ),
+  ),
+);
