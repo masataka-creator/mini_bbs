fc16b3c2fb804790f484f48cbc59ff79b4752e61
masataka-creator <masataka.asano.0101@gmail.com>
2021-02-02 22:37:14 +0900

    commit

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $db = new PDO('mysql:dbname=mini_bbs;host=localhost;charset=utf8','root','root');
+  $db = new PDO('mysql:dbname=heroku_07672f8095a1cdf;host=us-cdbr-east-03.cleardb.com;charset=utf8','b4517625d8bf13','d2650344');
 } catch(PDOException $e) {
   print('DB接続エラー：' . $e->getMessage());
 }
